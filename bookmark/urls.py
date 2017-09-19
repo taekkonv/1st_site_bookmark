b0c7adfe625ffa471c37261f14401a3fd477d9a3
Snowfinger <taekkonv@gmail.com>
2017-09-19 20:35:52 +0900

    My First App, First Commit

--- bookmark/urls.py
+++ bookmark/urls.py
@@ -1,10 +1,12 @@
-from django.conf.urls import url, include
+from django.conf.urls import url
 from django.contrib import admin
 
+from .views import *
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^bookmark/', include("bookmark.urls"))
+    url(r'^$', BookmarkListV.as_view(), name='index'),
+    url(r'^(?P<pk>\d+)/$', BookmarkDetailV.as_view(), name='detail')
 
     """
         정규표현식의 괄호는 그룹. 위의 괄호사이에 값은 이 패턴에서 어떤 키캆이 들어갈 것인지 결정해주는 정규표현식.
